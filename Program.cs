97381a4c67a438b4ad6e3e37e7e1e6ec683fc258
kingruneorb <kingruneorb@gmail.com>
2018-10-06 17:21:49 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,7 +10,48 @@ namespace TBA
     {
         static void Main(string[] args)
         {
+            //Ints
+            int correct = 0;
 
+            //Strings
+            string gender;
+            string Race;
+            //Character Creation
+            do
+            {
+                Console.WriteLine("Choose a gender:");
+                Console.WriteLine("Male / Female");
+                gender = Console.ReadLine().ToUpper();
+                if (gender == "MALE")
+                {
+                    correct = 1;
+                }
+                if (gender == "FEMALE")
+                {
+                    correct = 1;
+                }
+                else { }
+            } while (correct == 0);
+            correct = 0;
+
+            //Race Creation
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Choose a Race:");
+                Console.WriteLine("Human");
+                Console.WriteLine("Dwarf");
+                Console.WriteLine("Elf");
+                Console.WriteLine("Orc");
+                Console.WriteLine("Sips Fiend");
+                Race = Console.ReadLine().ToUpper();
+                if (Race == "Human" || Race == "Dwarf" || Race == "Elf" || Race == "Orc" || Race == "Sips Fiend")
+                {
+                    correct = 1;
+                }
+                else { }
+            } while (correct == 0);
+            correct = 0;
         }
     }
 }
